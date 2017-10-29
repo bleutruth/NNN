e3f8b186df07e8ef0b1474e48930d43d214062e3
bleutruth <bleutruth@gmail.com>
2017-10-29 17:15:17 +0900

    円の面積の計算をしてみた

--- 第1章_はじめよう/js-grammar/calc.js
+++ 第1章_はじめよう/js-grammar/calc.js
@@ -1,6 +1,13 @@
 (function(){
 	'use strict';
-	const x = 34;
-	const y = -0.25 * x + 14.1;
-	document.write(y);
+	{
+		const x = 34;
+		const y = -0.25 * x + 14.1;
+//		document.write(y);
+	}
+	{
+		const r = 12;
+		const x = r * r * 3.14;
+		document.write(x);
+	}
 })();
