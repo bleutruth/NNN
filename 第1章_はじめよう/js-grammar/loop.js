17bf6e2b29b1063830c81f3e87447a6e4a9f5c43
bleutruth <bleutruth@gmail.com>
2017-11-03 21:09:43 +0900

    整形した

--- 第1章_はじめよう/js-grammar/loop.js
+++ 第1章_はじめよう/js-grammar/loop.js
@@ -1,20 +1,20 @@
-(function(){
+(function () {
 	'use strict';
-	// for(var i = 1; i <= 100000; i++){
+	// for (var i = 1; i <= 100000; i++) {
 	// 	document.write(i + ' ');
 	// }
 
-	for(var i = 1; i <= 1000; i++){
-		if(i % 15 === 0){
+	for (var i = 1; i <= 1000; i++) {
+		if (i % 15 === 0) {
 			document.write('FizzBuzz, ');
 		}
-		else if(i % 5 === 0){
+		else if (i % 5 === 0) {
 			document.write('Buzz, ');
 		}
-		else if(i % 3 === 0){
+		else if (i % 3 === 0) {
 			document.write('Fizz, ');
 		}
-		else{
+		else {
 			document.write(i + ', ');
 		}
 	}
