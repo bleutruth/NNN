569a231d93ad3ecc811218edd915955b987de279
bleutruth <bleutruth@gmail.com>
2017-11-03 20:56:50 +0900

    1から1000までのFizzBuzz

--- 第1章_はじめよう/js-grammar/loop.js
+++ 第1章_はじめよう/js-grammar/loop.js
@@ -1,6 +1,21 @@
 (function(){
 	'use strict';
-	for(var i = 1; i <= 100000; i++){
-		document.write(i + ' ');
+	// for(var i = 1; i <= 100000; i++){
+	// 	document.write(i + ' ');
+	// }
+
+	for(var i = 1; i <= 1000; i++){
+		if(i % 15 === 0){
+			document.write('FizzBuzz, ');
+		}
+		else if(i % 5 === 0){
+			document.write('Buzz, ');
+		}
+		else if(i % 3 === 0){
+			document.write('Fizz, ');
+		}
+		else{
+			document.write(i + ', ');
+		}
 	}
 })();
