044fa80a5f018c5335f5500edd2b0646966f7005
bleutruth <bleutruth@gmail.com>
2017-11-03 15:17:56 +0900

    年齢と性別で分岐する処理を試す。

--- 第1章_はじめよう/js-grammar/logic.js
+++ 第1章_はじめよう/js-grammar/logic.js
@@ -1,7 +1,20 @@
 (function(){
 	'use strict';
 
-	const age = 19;
-	const result = (age >= 20) ? '成年' : '未成年';
-	document.write(result);
+	// const age = 19;
+	// const result = (age >= 20) ? '成年' : '未成年';
+	// document.write(result);
+
+	const age = 20;
+	const is_female = true;
+
+	if(age <= 15){
+		document.write('800円');
+	}
+	else if(is_female){
+		document.write('1000円');
+	}
+	else{
+		document.write('1800円');
+	}
 })();
