f44286084702e54380c8017f68d0022110e5601d
bleutruth <bleutruth@gmail.com>
2017-11-04 15:57:37 +0900

    任意の日時から現在までの時間を表示する処理を試す。

--- 第1章_はじめよう/js-grammar/function.js
+++ 第1章_はじめよう/js-grammar/function.js
@@ -1,3 +1,13 @@
 (function () {
 	'use strict';
+
+	const myBirthTime = new Date(2001, 1, 1, 12, 0);	// 適当な日付
+
+	function updateParagraph() {
+		const now = new Date();
+		const seconds = (now.getTime() - myBirthTime.getTime()) / 1000;
+		document.getElementById('birth-time').innerText = '生まれてから' + seconds + '秒経過。';
+	}
+
+	setInterval(updateParagraph, 100);
 })();
