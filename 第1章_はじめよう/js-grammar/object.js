8ef576bb5d25318d70f1679c0adcee2e3e76f646
bleutruth <bleutruth@gmail.com>
2017-11-04 17:06:14 +0900

    結果の判定と表示を行うようにした。

--- 第1章_はじめよう/js-grammar/object.js
+++ 第1章_はじめよう/js-grammar/object.js
@@ -1,15 +1,24 @@
 (function () {
 	'use strict';
 	var startTime = null;
+	const displayArea = document.getElementById('display-area');
 
 	function start() {
 		startTime = new Date().getTime();
 		document.body.onkeypress = stop;
-		console.log('スタートしました');
+		// console.log('スタートしました');
 	}
 
 	function stop() {
-		console.log('ストップしました');
+		// console.log('ストップしました');
+		const currentTime = new Date().getTime();
+		const seconds = (currentTime - startTime) / 1000;
+		if ((seconds >= 9.5) && (seconds <= 10.5)) {
+			displayArea.innerText = seconds + '秒でした。すごい。';
+		}
+		else {
+			displayArea.innerText = seconds + '秒でした。残念。';
+		}
 	}
 
 	if (confirm('OKを押して10秒だと思ったら何かキーを押してください')) {
