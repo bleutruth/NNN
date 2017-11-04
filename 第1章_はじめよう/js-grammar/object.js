67efec155893ccb3ea9f35036e7141ce643125cf
bleutruth <bleutruth@gmail.com>
2017-11-04 16:58:00 +0900

    何らかのキーが押された時にstopが呼ばれるようにした。

--- 第1章_はじめよう/js-grammar/object.js
+++ 第1章_はじめよう/js-grammar/object.js
@@ -4,7 +4,12 @@
 
 	function start() {
 		startTime = new Date().getTime();
+		document.body.onkeypress = stop;
 		console.log('スタートしました');
+	}
+
+	function stop() {
+		console.log('ストップしました');
 	}
 
 	if (confirm('OKを押して10秒だと思ったら何かキーを押してください')) {
