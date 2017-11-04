1debcfcc3345424e53a80f81be267cbdc545b944
bleutruth <bleutruth@gmail.com>
2017-11-04 16:50:28 +0900

    スタート時の時間を覚えておくようにした。

--- 第1章_はじめよう/js-grammar/object.js
+++ 第1章_はじめよう/js-grammar/object.js
@@ -1,8 +1,13 @@
 (function () {
 	'use strict';
+	var startTime = null;
+
+	function start() {
+		startTime = new Date().getTime();
+		console.log('スタートしました');
+	}
 
 	if (confirm('OKを押して10秒だと思ったら何かキーを押してください')) {
-		// TODOスタート処理
-		console.log('スタートしました');
+		start();
 	}
 })();
