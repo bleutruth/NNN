4f81ea33ddde75793983e85279670343cc92b4ac
bleutruth <bleutruth@gmail.com>
2017-11-04 16:10:30 +0900

    秒数表示を日数表示に変更。

--- 第1章_はじめよう/js-grammar/function.js
+++ 第1章_はじめよう/js-grammar/function.js
@@ -1,12 +1,13 @@
 (function () {
 	'use strict';
 
+	const oneDay = 60 * 60 * 24;	// 1日の秒数
 	const myBirthTime = new Date(2001, 1, 1, 12, 0);	// 適当な日付
 
 	function updateParagraph() {
 		const now = new Date();
-		const seconds = (now.getTime() - myBirthTime.getTime()) / 1000;
-		document.getElementById('birth-time').innerText = '生まれてから' + seconds + '秒経過。';
+		const day = (now.getTime() - myBirthTime.getTime()) / (oneDay * 1000);
+		document.getElementById('birth-time').innerText = '生まれてから' + day + '日経過。';
 	}
 
 	setInterval(updateParagraph, 100);
