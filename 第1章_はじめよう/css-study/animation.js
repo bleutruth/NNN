b4a28561f4c1d366f8e1d19e70a12561e65530d4
bleutruth <bleutruth@gmail.com>
2017-11-11 19:36:32 +0900

    回転時の角度によってクラスを切り替えるようにした。

--- 第1章_はじめよう/css-study/animation.js
+++ 第1章_はじめよう/css-study/animation.js
@@ -6,6 +6,13 @@
 
 	function rotateHeader() {
 		deg += 6;
+		deg %= 360;
+		if (((deg >= 0) && (deg < 90)) || ((deg >= 270) && (deg < 360))) {
+			header.className = 'face';
+		}
+		else {
+			header.className = 'back';
+		}
 		header.style.transform = 'rotateX(' + deg + 'deg)';
 	}
 	setInterval(rotateHeader, 20);
