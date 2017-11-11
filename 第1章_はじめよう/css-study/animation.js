614f02385e291ee6c882069d510805edeec5700b
bleutruth <bleutruth@gmail.com>
2017-11-11 19:23:19 +0900

    X軸回転するアニメーションをつけてみた。

--- 第1章_はじめよう/css-study/animation.js
+++ 第1章_はじめよう/css-study/animation.js
@@ -1,5 +1,12 @@
 (function () {
 	'use strict';
+
 	const header = document.getElementById('header');
-	header.style.transform = 'rotateX(60deg)';
+	var deg = 0;
+
+	function rotateHeader() {
+		deg += 6;
+		header.style.transform = 'rotateX(' + deg + 'deg)';
+	}
+	setInterval(rotateHeader, 20);
 })();
