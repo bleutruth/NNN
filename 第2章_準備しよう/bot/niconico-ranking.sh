bac77d9809762a8391a710685c108b4b78f1ef47
bleutruth <bleutruth@gmail.com>
2017-12-10 20:31:28 +0900

    RSSを取得するようにした。
    メールアドレス部分は自分のものに手直しする必要がある。

--- 第2章_準備しよう/bot/niconico-ranking.sh
+++ 第2章_準備しよう/bot/niconico-ranking.sh
@@ -5,3 +5,5 @@ mkdir -p $dirname
 
 filename="${dirname}/hourly-ranking-`date +'%Y%m%d%H%M'`.xml"
 echo "Save to $filename"
+
+curl -s -o $filename -H "User-Agent: CrawlBot; your@mail" http://www.nicovideo.jp/ranking/fav/hourly/all?rss=2.0&lang=ja-jp
