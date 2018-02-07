33260c53c86c916f890fac5911da4473aa3116dd
bleutruth <bleutruth@gmail.com>
2018-02-08 00:11:56 +0900

    TODOを追加する処理を追加。

---
+++ Chapter3/todo/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// key: タスクの文字列, value: 完了しているかどうかの真偽値
+const tasks = new Map();
+
+/**
+ * TODOを追加する
+ * @param {string} task
+ */
+function todo(task) {
+	tasks.set(task, false);
+}
+
+module.exports = {
+	todo: todo
+};
