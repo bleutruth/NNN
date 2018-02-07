12a9ff95e7961b4db16d5c0136564cbd68e1d3d1
bleutruth <bleutruth@gmail.com>
2018-02-08 00:41:23 +0900

    todoとlistのテストを追加。

--- Chapter3/todo/test.js
+++ Chapter3/todo/test.js
@@ -1,3 +1,11 @@
 'use strict';
+
 const todo = require('./index.js');
+const assert = require('assert');
+
+// todo と list のテスト
+todo.todo('ノートを買う');
+todo.todo('鉛筆を買う');
+assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+
 console.log('テストが正常に完了しました');
