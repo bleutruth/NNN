815808637b5a1537a3cf2a2a1ab6872e8cf37727
bleutruth <bleutruth@gmail.com>
2018-02-17 15:43:02 +0900

    list, donelistのコマンドに対応。

--- Chapter3/hubot-todo/scripts/todo.js
+++ Chapter3/hubot-todo/scripts/todo.js
@@ -27,4 +27,10 @@ module.exports = (robot) => {
 		todo.done(task);
 		msg.send('削除しました:' + task);
 	});
+	robot.respond(/list/i, (msg) => {
+		msg.send(todo.list().join('\n'));
+	});
+	robot.respond(/donelist/i, (msg) => {
+		msg.send(todo.donelist().join('\n'));
+	});
 };
