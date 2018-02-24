4c9b2458dc3c3c869db454b299a325fe3cff4560
bleutruth <bleutruth@gmail.com>
2018-02-24 20:56:58 +0900

    nodeの非同期処理を試す。
    順序が守られずにファイルに出力される。

---
+++ Chapter3/async-io-problem/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const fs = require('fs');
+const fileName = './test.txt';
+
+for (let i = 0; i < 500; i++) {
+	fs.appendFile(fileName, 'あ', 'utf8');
+	fs.appendFile(fileName, 'い', 'utf8');
+	fs.appendFile(fileName, 'う', 'utf8');
+	fs.appendFile(fileName, 'え', 'utf8');
+	fs.appendFile(fileName, 'お', 'utf8');
+	fs.appendFile(fileName, '\n', 'utf8');
+}
